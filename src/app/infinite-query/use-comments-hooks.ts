501f529f2659d0e3d6eb81b218458c942c55a377
Mustafa Güzel <mustafaguzel879@gmail.com>
2025-03-31 13:27:22 +0300

    Updated the cache using setQueryData

--- src/app/infinite-query/use-comments-hooks.ts
+++ src/app/infinite-query/use-comments-hooks.ts
@@ -1,8 +1,11 @@
-import { useInfiniteQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { fetchData, postData } from "@/lib/fetch-utils";
 import { CommentsResponse } from "../api/comments/route";
 import { Comment } from "../api/comments/data";
-
 
 export function useCommentsQuery() {
   return useInfiniteQuery({
@@ -20,9 +23,15 @@ export function useCreateCommentMutation() {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (newComment: { text: string }) =>
-      postData<Comment>("/api/comments", newComment),
-    onSuccess: () => {
+      postData<{ comment: Comment }>("/api/comments", newComment),
+    onSuccess: ({ comment }) => {
       queryClient.invalidateQueries({ queryKey: ["comments"] });
+      queryClient.setQueryData(["comments"], (oldData: CommentsResponse) => {
+        return {
+          ...oldData,
+          comments: [comment, ...oldData.comments],
+        };
+      });
     },
   });
 }
